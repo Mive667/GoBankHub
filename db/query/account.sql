96a73d554f61e9bbafa8d300de8b49c56bd8d0b9
yulin_luo <luoyulin9103@gmail.com>
2024-01-29 20:57:08 -0800

    config READ operation

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,13 @@ INSERT INTO account (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM account
+ORDER BY id
+LIMIT $1
+OFFSET $2;
